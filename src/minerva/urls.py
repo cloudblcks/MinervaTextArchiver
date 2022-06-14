41408db115fe226ea471805538088a12f3975533
Roy Lewin <roy.lewin@gmail.com>
2022-06-14 19:14:10 +0100

    Add heartbeat endpoints using django-alive

--- src/minerva/urls.py
+++ src/minerva/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
 
 from minerva import settings
@@ -23,6 +23,8 @@ from minerva.webapp.views import DiscussionStatsView, AppGroupStatsView, Discuss
     UserHashtagsView, UserRegisterView, UserAddAppView, UserDetailsView
 
 urlpatterns = [
+    path("-/", include("django_alive.urls")),
+
     path('admin/', admin.site.urls),
 
     path('webapp/discussions/stats', DiscussionStatsView.as_view(), name='discussion_stats'),
